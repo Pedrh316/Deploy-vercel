6ab9d5b24e5d74230e157f2c73abac76545073b7
Pedro <pedrh315@gmail.com>
2022-12-21 20:04:01 -0300

    adicionando parágrafo na pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 export default function Home(){
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <p>Conteúdo adicionado na branch main</p>
+        </div>
     )
 }
